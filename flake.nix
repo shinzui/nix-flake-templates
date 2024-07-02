f6d8ca03721b4628c9b1bf1fca5e4215e3129603
Nadeem Bitar <nadeem@gmail.com>
2024-07-01 21:27:29 -0700

    📜 Add ghc 9.10 template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./haskell;
         description = "A template for haskell development";
       };
+      haskell-9_10 = {
+        path = ./haskell-9_10;
+        description = "A template for haskell development with GHC 9.10";
+      };
       haskell-9_4 = {
         path = ./haskell-9_4;
         description = "A template for haskell development with GHC 9.4";
---
+++ haskell-9_10/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Haskell nix template";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+
+
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils, treefmt-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ghcVersion = "ghc9101";
+        frameworks = pkgs.darwin.apple_sdk.frameworks;
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        formatter = treefmtEval.config.build.wrapper;
+
+        # haskellPackages = pkgs.haskell.packages."${ghcVersion}";
+      in
+      {
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              treefmt.package = formatter;
+              treefmt.enable = true;
+
+            };
+          };
+        };
+        devShell = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = [
+            frameworks.Cocoa
+          ];
+          nativeBuildInputs = [
+            pkgs.zlib
+            pkgs.xz
+            pkgs.just
+            pkgs.cabal-install
+            pkgs.haskell.packages."${ghcVersion}".haskell-language-server
+            pkgs.haskell.compiler."${ghcVersion}"
+          ];
+        };
+      }
+    );
+}
