b6a18a1927d2561da02fd05ea2b06bdbabfa40ee
Nadeem Bitar <nadeem@gmail.com>
2025-02-10 07:53:04 -0800

    🔥 Remove ghc 9.4 template

--- flake.nix
+++ flake.nix
@@ -11,10 +11,6 @@
         path = ./haskell-9_10;
         description = "A template for haskell development with GHC 9.10";
       };
-      haskell-9_4 = {
-        path = ./haskell-9_4;
-        description = "A template for haskell development with GHC 9.4";
-      };
       haskell-hub = {
         path = ./haskell-hub;
         description = "A template for haskell development in random open source projects";
