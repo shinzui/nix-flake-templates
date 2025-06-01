cf3ca6cd540158b010e1e38d6400967553e3472a
Nadeem Bitar <nadeem@gmail.com>
2025-06-01 08:28:30 -0700

    Add ghc 9.12 template

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
         path = ./haskell-9_10;
         description = "A template for haskell development with GHC 9.10";
       };
+      haskell-9_12 = {
+        path = ./haskell-9_12;
+        description = "A template for haskell development with GHC 9.12";
+      };
       haskell-hub = {
         path = ./haskell-hub;
         description = "A template for haskell development in random open source projects";
