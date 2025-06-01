ca38d45cd4ab31bc0831f3cd71325746f62f8750
Nadeem Bitar <nadeem@gmail.com>
2025-06-01 10:45:43 -0700

    Configure treefmt in nodejs template

--- nodejs/flake.nix
+++ nodejs/flake.nix
@@ -4,15 +4,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
-
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, treefmt-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        formatter = treefmtEval.config.build.wrapper;
       in
       {
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              treefmt.package = formatter;
+              treefmt.enable = true;
+            };
+          };
+        };
         devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
           buildInputs = with pkgs; [
             nodejs_22
             pnpm
